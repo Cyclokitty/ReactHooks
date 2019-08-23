e065188c944f01379061d1cc9ffb24fd7ec70c09
Cyclokitty <laura@vingada.com>
2019-08-23 18:27:15 -0400

    corrected how to use TextInput and used secureTextEntry for password style TextInput

--- src/screens/FormScreen.js
+++ src/screens/FormScreen.js
@@ -3,17 +3,31 @@ import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 const FormScreen = () => {
     const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Form Screen</Text>
-            <Text style={styles.inputTitle}>Your name is: {name}</Text>
+            <Text style={styles.inputTitle}>Your name is: </Text>
             <TextInput
                 style={styles.input}
-                onChangeText={ ({name}) => setName(name) }
+                onChangeText={ (newText) => setName(newText) }
                 value={name}
+                autoCapitalize='words'
+                autoCorrect={false}
             />
-            
+            <Text style={styles.inputTitle}>{name}</Text>
+
+            <Text style={styles.inputTitle}>Set Your Password: </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={ (newText) => setPassword(newText) }
+                value={password}
+                autoCapitalize='none'
+                autoCorrect={false}
+                secureTextEntry={true}
+            />
+            { password.length < 5 ? <Text style={styles.inputTitle}>Password must be longer than 5 characters</Text> : null}
         </View>
     )
 };
